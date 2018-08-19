bee4de4f3dde55140f5f20e5bb5b8182d311a631
tawkira.786@gmail.com <tawkira.786@gmail.com>
2018-08-19 23:29:39 +0530

    Module Driven form example

--- src/app/md-form/md-form.component.ts
+++ src/app/md-form/md-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from '../user';
 
 @Component({
   selector: 'app-md-form',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MdFormComponent implements OnInit {
 
+  private genderList = ['Male', 'Female'];
+  private user: User;
+  signupForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.signupForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
+      password: new FormGroup({
+        pwd: new FormControl('', [Validators.required, Validators.minLength(8)]),
+        confirmPwd: new FormControl('', [Validators.required, Validators.minLength(8)])
+      }),      
+      gender: new FormControl('', Validators.required),
+      terms: new FormControl('', Validators.requiredTrue)
+    })
   }
+
+  get email() {
+    return this.signupForm.get('email');
+  }
+
+  get password() {
+    return this.signupForm.get('password');
+  }
+
+  get gender() {
+    return this.signupForm.get('gender');
+  }
+
+  get terms() {
+    return this.signupForm.get('terms');
+  }
+
+  if(this.signupForm.valid) {
+    this.user = this.signupForm.value;
+    console.log(this.user);
+    /* Any API call logic via services goes here */
+  }
+
 
 }
